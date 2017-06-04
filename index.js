8545464f8b57774d1890615895cb97233ec6ede3
Shubham Thakur <sbmthakur@gmail.com>
2017-06-04 23:24:22 +0530

    Fetch synonyms and antonyms for the word too
    requestSynsAnts does the job of fetching synonyms and antonyms, the
    response of define is slightly formatted so that it looks good to human eyes

--- index.js
+++ index.js
@@ -13,11 +13,62 @@ const options = {
   "proxy": config.proxy
    };
 
+const print = function(someString, result){ 
+    return console.log("\n" + someString, result);
+};
+
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
      const wordData = JSON.parse(body); 
-     console.log("You asked for: ", word);
-     console.log("Meaning: ", JSON.parse(body).results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]);
-     console.log("Example: ", JSON.parse(body).results[0].lexicalEntries[0].entries[0].senses[0].examples[0].text);
+     print("You asked for: ", word);
+     print("Meaning: ", JSON.parse(body).results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]);
+     print("Example: ", JSON.parse(body).results[0].lexicalEntries[0].entries[0].senses[0].examples[0].text);
+     requestSynsAnts(word, (err, wordData) => {
+
+       print("Synonyms: ", wordData.synonyms.replace(/, $/,''));
+       print("Antonyms: ", wordData.antonyms.replace(/, $/,''));
+     }); 
 });
 
+function requestSynsAnts(word, callback){
+
+		const options = { 
+     "method": "GET",
+     "url": config.baseUrl + word + "/synonyms;antonyms",
+     "headers": { 
+        "app_key": config.appHeaders.key,
+        "app_id": config.appHeaders.id 
+      },
+     "proxy": config.proxy
+    };
+    print("Fetching synonyms and antonyms","...."); 
+    request(options, function (error, response, body) {
+
+        const senses = JSON.parse(body).results[0].lexicalEntries[0].entries[0].senses;
+        let synonyms = "";
+        let antonyms = "";
+        for(let sense of senses){
+
+            if(sense.hasOwnProperty("synonyms")){
+            
+              sense["synonyms"].map(synonym => {
+                      
+                  synonyms += synonym.text + ", ";
+              }); 
+            }
+            if(sense.hasOwnProperty("antonyms")){
+            
+              sense["antonyms"].map(antonym => {
+                      
+                  antonyms += antonym.text + ", ";
+              }); 
+            }
+        }
+
+        const wordData = {
+            "synonyms": synonyms, 
+            "antonyms": antonyms
+        }
+       return callback(null, wordData);
+    });
+}
