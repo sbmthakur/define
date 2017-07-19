2daad27886e7ce49064a7a4856a2129a9e61dba0
Shubham Thakur <sbmthakur@gmail.com>
2017-07-19 13:43:17 +0530

    Format index.js as per linter

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const request = require("request");
 const config = require("./conf.json");
 const print = function(someString, result){ 
-    return console.log("\n" + someString, result || '');
+    return console.log("\n" + someString, result || "");
 };
 
 const word = process.argv[2];
@@ -16,30 +16,30 @@ if(!word) {
 
 const Redis = require("redis");
 const redis = Redis.createClient({
-               "host": config.redis.host,
-               "port": config.redis.port
-              });
+    "host": config.redis.host,
+    "port": config.redis.port
+});
 
 wordExistsRedis(word, (error, result) => {
-   if(error){
-       print("Some problem with redis server", error);
-       redis.quit();
-   }
-   else{
-       if(result === null){
-           requestWordInfo(word); 
-       }
-       else{
+    if(error){
+        print("Some problem with redis server", error);
+        redis.quit();
+    }
+    else{
+        if(result === null){
+            requestWordInfo(word); 
+        }
+        else{
 
-         const wordData = JSON.parse(result);
-         printWordData(wordData, () => {
+            const wordData = JSON.parse(result);
+            printWordData(wordData, () => {
                  
-           print("Synonyms: ", wordData.synonyms);
-           print("Antonyms: ", wordData.antonyms);
-           redis.quit();
-         });
-       }
-   }
+                print("Synonyms: ", wordData.synonyms);
+                print("Antonyms: ", wordData.antonyms);
+                redis.quit();
+            });
+        }
+    }
 });
 
 function wordExistsRedis(word, callback){
@@ -57,57 +57,57 @@ function wordExistsRedis(word, callback){
 function requestWordInfo(word){
 
     const options = { 
-      "method": "GET",
-      "url": config.basePath + word,
-      "headers": { 
-         "app_key": config.appHeaders.key,
-         "app_id": config.appHeaders.id 
-       }
+        "method": "GET",
+        "url": config.basePath + word,
+        "headers": { 
+            "app_key": config.appHeaders.key,
+            "app_id": config.appHeaders.id 
+        }
     };
     request(options, function (error, response, body) {
-      if (error) throw new Error(error);
+        if (error) throw new Error(error);
 
-      if (response.statusCode !== 200) {
-          redis.quit();
-          if (response.statusCode === 404)
-              return print("This word doesn't exist in oxford dictionary"); 
-          else
+        if (response.statusCode !== 200) {
+            redis.quit();
+            if (response.statusCode === 404)
+                return print("This word doesn't exist in oxford dictionary"); 
+            else
               return print("Some problem with API server"); 
-      }
+        }
         let redisData = JSON.parse(body); 
         printWordData(redisData, () => {
-         requestSynsAnts(word, (err, wordData) => {
+            requestSynsAnts(word, (err, wordData) => {
     
-           let synonyms = wordData.synonyms.replace(/, $/,'');
-           let antonyms = wordData.antonyms.replace(/, $/,''); 
-           print("Synonyms: ", synonyms);
-           print("Antonyms: ", antonyms);
-           redisData["synonyms"] = synonyms;
-           redisData["antonyms"] = antonyms;
+                let synonyms = wordData.synonyms.replace(/, $/,"");
+                let antonyms = wordData.antonyms.replace(/, $/,""); 
+                print("Synonyms: ", synonyms);
+                print("Antonyms: ", antonyms);
+                redisData["synonyms"] = synonyms;
+                redisData["antonyms"] = antonyms;
 
-           storeDataRedis(word, JSON.stringify(redisData), (err, res) => {
-               if(err){
-                   print("Couldn't store this word in redis", err); 
-               }
-               else{
-                   print("Word data added to cache"); 
-               }
-               redis.quit();
-           });
-         }); 
+                storeDataRedis(word, JSON.stringify(redisData), (err, res) => {
+                    if(err){
+                        print("Couldn't store this word in redis", err); 
+                    }
+                    else{
+                        print("Word data added to cache"); 
+                    }
+                    redis.quit();
+                });
+            }); 
         });
     });
 }
 
 function requestSynsAnts(word, callback){
 
-		const options = { 
-     "method": "GET",
-     "url": config.basePath + word + "/synonyms;antonyms",
-     "headers": { 
-        "app_key": config.appHeaders.key,
-        "app_id": config.appHeaders.id 
-      }
+    const options = { 
+        "method": "GET",
+        "url": config.basePath + word + "/synonyms;antonyms",
+        "headers": { 
+            "app_key": config.appHeaders.key,
+            "app_id": config.appHeaders.id 
+        }
     };
     print("Fetching synonyms and antonyms","...."); 
     request(options, function (error, response, body) {
@@ -118,11 +118,11 @@ function requestSynsAnts(word, callback){
          
           //404 is handled below with an 'or' operator when 'wordData' variable is assigned
 
-          if (response.statusCode >= 500){
+            if (response.statusCode >= 500){
           
-              print("Some problem with API server. Please request the word when the server is up."); 
-              process.exit();
-          }
+                print("Some problem with API server. Please request the word when the server is up."); 
+                process.exit();
+            }
 
         }
         else {
@@ -132,17 +132,17 @@ function requestSynsAnts(word, callback){
 
                 if(sense.hasOwnProperty("synonyms")){
                 
-                  sense["synonyms"].map(synonym => {
+                    sense["synonyms"].map(synonym => {
                           
-                      synonyms += synonym.text + ", ";
-                  }); 
+                        synonyms += synonym.text + ", ";
+                    }); 
                 }
                 if(sense.hasOwnProperty("antonyms")){
                 
-                  sense["antonyms"].map(antonym => {
+                    sense["antonyms"].map(antonym => {
                           
-                      antonyms += antonym.text + ", ";
-                  }); 
+                        antonyms += antonym.text + ", ";
+                    }); 
                 }
             }
         }
@@ -153,8 +153,8 @@ function requestSynsAnts(word, callback){
 
             "synonyms": synonyms || "none", 
             "antonyms": antonyms || "none"
-        }
-       return callback(null, wordData);
+        };
+        return callback(null, wordData);
     });
 }
 
@@ -173,19 +173,19 @@ function storeDataRedis(word, redisData, callback){
 
 function printWordData(wordData, callback){
 
-         print("You asked for: ", wordData.results[0].id);
-         wordData.results[0].lexicalEntries.map(wordInfo => {
-         print(wordInfo.lexicalCategory);
-         print("Meaning: ", wordInfo.entries[0].senses[0].definitions[0]);
-         try{
+    print("You asked for: ", wordData.results[0].id);
+    wordData.results[0].lexicalEntries.map(wordInfo => {
+        print(wordInfo.lexicalCategory);
+        print("Meaning: ", wordInfo.entries[0].senses[0].definitions[0]);
+        try{
          
             print("Example: ", wordInfo.entries[0].senses[0].examples[0].text);
-         }
-         catch(e){
-           print("Couldn't find any example.");
-         }
+        }
+        catch(e){
+            print("Couldn't find any example.");
+        }
 
-         });
-         callback();
+    });
+    callback();
 }
 
