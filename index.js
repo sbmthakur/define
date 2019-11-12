136187a8616349c0455e482df98fc9ebe39cee49
Shubham Thakur <sbmthakur@gmail.com>
2019-11-12 23:16:21 +0530

    Make caching optional

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let redis = new Redis.default({
 const print = function(someString, result) {
   return console.log('\n' + someString, result || '');
 };
+
 async function wordExistsRedis(word) {
   let wordKey = 'word:' + word;
   return redis.get(wordKey);
@@ -51,23 +52,41 @@ const axios = require('axios');
 const config = require('./conf.json');
 
 (async () => {
-  const word = process.argv[2].toLowerCase();
-  let result = await wordExistsRedis(word);
-  if (result === null) {
-    await requestWordInfo(word);
+  let word, cache = false;
+
+  if(process.argv.length > 3) {
+    if(process.argv[2] !== '-c') {
+      return print('Inconsistent input');
+    }
+    cache = true;
+    word = process.argv[3].toLowerCase();
   } else {
-    const wordData = JSON.parse(result);
-    printWordData(wordData, (exception) => {
-      if (exception) {
-        console.log('Exception: ', exception);
-      }
-    });
+    word = process.argv[2].toLowerCase();
   }
+
+  if (cache) {
+    let result = await wordExistsRedis(word);
+
+    if(result === null) {
+      await requestWordInfo(word, true);
+    } else {
+      const wordData = JSON.parse(result);
+      printWordData(wordData, (exception) => {
+        if (exception) {
+          console.log('Exception: ', exception);
+        }
+      });
+    }
+
+  } else {
+    await requestWordInfo(word, false);
+  }
+
   redis.quit();
 })();
 
 
-async function requestWordInfo(word) {
+async function requestWordInfo(word, cache) {
   try {
     let url = `${config.basePath}/entries/${config.sourceLang}/${word}?strictMatch=false`;
     const options = {
@@ -86,11 +105,14 @@ async function requestWordInfo(word) {
         console.log('Exception', exception);
       }
     });
-    await storeDataRedis(word, JSON.stringify(redisData));
+
+    if(cache) {
+      await storeDataRedis(word, JSON.stringify(redisData));
+    }
   } catch(err) {
     let status = err.response.status;
     if (status === 404) {
-      return print('This word doesn\'t exist in oxford dictionary');
+      return print('This word doesn\'t exist in Oxford dictionary');
     }
     else {
       return print('Some problem with API server', err.message);
